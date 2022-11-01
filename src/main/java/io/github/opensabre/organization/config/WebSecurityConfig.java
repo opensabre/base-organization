8f6250ed765872c43c0a1e90499e16f378e724dc
周涛 <zhoutaoo@foxmail.com>
2022-11-02 00:57:26 +0800

    增加接口签权功能

---
+++ src/main/java/io/github/opensabre/organization/config/WebSecurityConfig.java
@@ -0,0 +1,51 @@
+package io.github.opensabre.organization.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain httpSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeRequests().anyRequest().authenticated()
+                .and().cors()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter());
+        return httpSecurity.build();
+    }
+
+    /**
+     * 从 JWT 的 scope 中获取的权限 取消 SCOPE_ 的前缀
+     * 设置从 jwt claim 中那个字段获取权限
+     * 如果需要同多个字段中获取权限或者是通过url请求获取的权限，则需要自己提供jwtAuthenticationConverter()这个方法的实现
+     *
+     * @return JwtAuthenticationConverter
+     */
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        // 去掉 SCOPE_ 的前缀
+        authoritiesConverter.setAuthorityPrefix("");
+        // 从jwt claim 中那个字段获取权限，模式是从 scope 或 scp 字段中获取
+        authoritiesConverter.setAuthoritiesClaimName("scope");
+        converter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+        return converter;
+    }
+
+}
