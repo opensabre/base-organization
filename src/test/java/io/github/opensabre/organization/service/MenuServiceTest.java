888e3f6dc566096922970bf9ac7795701437febe
周涛 <zhoutaoo@foxmail.com>
2026-07-18 17:16:37 +0800

    chore: 升级框架至0.5.0

--- src/test/java/io/github/opensabre/organization/service/MenuServiceTest.java
+++ src/test/java/io/github/opensabre/organization/service/MenuServiceTest.java
@@ -24,7 +24,7 @@ class MenuServiceTest {
         assertThat(menus.get(0).getChildren()).extracting(MenuVo::getId).containsExactly("102", "103", "104", "105", "106", "107");
         assertThat(menus.get(1).getChildren()).extracting(MenuVo::getId).containsExactly("110", "116");
         assertThat(menus.get(1).getChildren().get(1).getHref()).isEqualTo("/auth/online-user");
-        assertThat(menus.get(2).getChildren()).extracting(MenuVo::getId).containsExactly("111", "112", "113", "114", "115", "119");
+        assertThat(menus.get(2).getChildren()).extracting(MenuVo::getId).containsExactly("111", "112", "113", "114", "115", "119", "120", "121");
         assertThat(menus.get(2).getHref()).isEqualTo("/sysadmin");
         assertThat(menus.get(2).getChildren().get(0).getHref()).isEqualTo("/sysadmin/audit-log");
         assertThat(menus.get(3).getChildren()).extracting(MenuVo::getId).containsExactly("118");
