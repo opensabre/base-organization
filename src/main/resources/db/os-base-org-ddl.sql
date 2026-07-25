41d0ebb0a7265563530f7e2f0c7ce4216a669060
周涛 <zhoutaoo@foxmail.com>
2026-07-25 14:42:54 +0000

    feat: 新增Token管理菜单权限

--- src/main/resources/db/os-base-org-ddl.sql
+++ src/main/resources/db/os-base-org-ddl.sql
@@ -408,3 +408,15 @@ INSERT INTO base_org_role_menu (id, role_id, menu_id, created_time, updated_time
 SELECT 100000 + r.id * 1000 + m.id, r.id, m.id, now(), now(), 'system', 'system'
 FROM base_org_menu m JOIN base_org_role r ON r.id IN (101, 103)
 WHERE m.id IN (122, 123) OR m.id BETWEEN 130 AND 159;
+
+
+-- OAuth2授权记录管理
+INSERT INTO base_org_menu (id, parent_id, type, href, icon, name, description, order_num, created_time, updated_time, created_by, updated_by)
+VALUES (220, 108, 'MENU', '/auth/authorization', 'key', 'Token管理', '{"routeName":"OAuthAuthorization","component":"auth/authorization/index","visible":1}', 20, now(), now(), 'system', 'system'),
+       (221, 220, 'BUTTON', '', '', '撤销OAuth2授权', '{"perm":"auth:authorization:revoke"}', 1, now(), now(), 'system', 'system');
+
+INSERT INTO base_org_role_menu (id, role_id, menu_id, created_time, updated_time, created_by, updated_by)
+VALUES (101220, 101, 220, now(), now(), 'system', 'system'),
+       (103220, 103, 220, now(), now(), 'system', 'system'),
+       (101221, 101, 221, now(), now(), 'system', 'system'),
+       (103221, 103, 221, now(), now(), 'system', 'system');
